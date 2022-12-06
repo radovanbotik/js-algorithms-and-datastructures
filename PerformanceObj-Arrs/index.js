6761b6c527066a3144b379c2184acca4e059b17a
radovanbotik <radovanbotik@gmail.com>
2022-12-06 18:40:14 +0100

    ''

---
+++ PerformanceObj-Arrs/index.js
@@ -0,0 +1,10 @@
+let person = {
+  name: "john",
+  isBald: true,
+  favoriteNumbers: [1, 5, 10],
+};
+const animals = ["cat", "dog", "mouse"];
+
+//animals[2]
+// is constant we are not looping through every entry
+// we specifically are saying to get this one item under key of 2
